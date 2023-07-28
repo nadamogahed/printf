b7cf4ecac228b5d8d34c7c049200fa379d7395e3
nadamogahed <nadamarcy25@gmail.com>
2023-07-28 05:18:31 -0700

    betty

--- print_int.c
+++ print_int.c
@@ -4,15 +4,21 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "main.h"
-void print_int_c (int x)
+/*
+ * print_int_c - prints an integer
+ * @x: input integer
+ * Return: void
+ */
+void print_int_c(int x)
 {
-    if (x < 0) {
-                    _putchar('-');
-                    x = -x;
-                }
-                if (x / 10 > 0)
-                {
-                    _printf("%d", x / 10);
-                }
-                _putchar(x % 10 + '0');
+	if (x < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if (x / 10 > 0)
+	{
+		_printf("%d", x / 10);
+	}
+	_putchar(x % 10 + '0');
 }
--- printf.c
+++ printf.c
@@ -11,61 +11,45 @@
  */
 int _printf(const char *format, ...)
 {
-    int i, x, count;
-    char ch;
-    char *string;
-    int len = 0;
-    va_list args;
-    va_start(args, format);
+	int i, x, count, len;
+	char ch, *string;
+	va_list args;
 
-    len = length(format);
-    if (format == NULL)
-    {
-        return -1;
-    }
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        
-        if (format[i] == '%')
-        {
-            
-            i++;
-            if (format[i] == 'c')
-            {
-                ch = va_arg(args, int);
-                _putchar(ch);
-            }
-            else if (format[i] == 's')
-            {
-                string = va_arg(args, char*);
-                count = length(string);
-                print_string(string, count);
-            }
-            else if (format[i] == 'd' || format[i] == 'i')
-            {
-                x = va_arg(args, int);
-                print_int_c (x);
-            }    
-            else if (format[i] == '%')
-            {
-                _putchar('%');
-            }
-            else
-            {
-                write(1, &format[i], 1);
-            }
-        }
-        else if (format[i] == '\\' && format[i+1] == '\n')
-        {
-            _putchar(10);
-        }
-        else
-        {
-		    write(1, &format[i], 1);
-        }
-    }
-    va_end(args);
-    return(len);
+	va_start(args, format);
+	len = length(format);
+	if (format == NULL)
+		return (-1);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				ch = va_arg(args, int);
+				_putchar(ch);
+			}
+			else if (format[i] == 's')
+			{
+				string = va_arg(args, char*);
+				count = length(string);
+				print_string(string, count);
+			}
+			else if (format[i] == 'd' || format[i] == 'i')
+			{
+				x = va_arg(args, int);
+				print_int_c(x);
+			}
+			else
+				write(1, &format[i], 1);
+		}
+		else if (format[i] == '\\' && format[i + 1] == '\n')
+			_putchar(10);
+		else
+			write(1, &format[i], 1);
+	}
+	va_end(args);
+	return (len);
 }
 
 
