caf3ac2d0a261e058ff14e7243a1195cd0025969
Nada Mogahed <67779643+nadamogahed@users.noreply.github.com>
2023-07-29 02:40:47 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -33,14 +33,14 @@ int _printf(const char *format, ...)
             if (format[i] == 'c')
             {
                 ch = va_arg(args, int);
-                count += _putchar(ch);
+                count = _putchar(ch);
             }
             else if (format[i] == 's')
             {
                 string = va_arg(args, char*);
                 if (string == NULL)
                 {
-                    count += write(STDOUT_FILENO, "(null)", 6);
+                    count = write(STDOUT_FILENO, "(null)", 6);
                 }
                 else 
                 {
