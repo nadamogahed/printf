a0fc5d27b50c0fdcd6d73d7bc4fbe706362612ee
nadamogahed <nadamarcy25@gmail.com>
2023-07-28 04:07:32 -0700

    printf

--- printf.c
+++ printf.c
@@ -22,8 +22,13 @@ int _printf(const char *format, ...)
     {
         len++;
     }
+    if (format == NULL)
+    {
+        return -1; // or whatever error code is appropriate
+    }
     for (i = 0; format[i] != '\0'; i++)
     {
+        
         if (format[i] == '%')
         {
             
@@ -35,36 +40,43 @@ int _printf(const char *format, ...)
             }
             else if (format[i] == 's')
             {
-                string = va_arg(args, char*);
-                ptr = string;
-
-                    while (*ptr != '\0')
-                    {
-                        _putchar(*ptr);
-                        ptr++;
-                    }
-                
+                char *string = va_arg(args, char*);
+                int count = 0;
+                while (string[count] != '\0') {
+                    count++;
+                }
+                char *ptr = (char *) malloc((count + 1) * sizeof(char));
+                if (ptr == NULL) {
+                    // Error handling code
+                    return -1;
+                }
+                for (int j = 0; j < count; j++) {
+                    ptr[j] = string[j];
+                }
+                ptr[count+1] = '\0';
+                write(STDOUT_FILENO, ptr, count);
+                free(ptr);
             }
             else if (format[i] == 'd' || format[i] == 'i')
             {
-                    x = va_arg(args, int);
-                        if (x < 0) {
-                            _putchar('-');
-                            x = -x;
-                        }
-                        if (x / 10 > 0)
-                        {
-                            _putchar(x / 10);
-                        }
-                        _putchar(x % 10 + '0');
-                        i++;
-                        continue;
+                x = va_arg(args, int);
+                if (x < 0) {
+                    putchar('-');
+                    x = -x;
+                }
+                if (x / 10 > 0)
+                {
+                    _printf("%d", x / 10);
+                }
+                putchar(x % 10 + '0');
             }    
             else if (format[i] == '%')
             {
-                _putchar(format[i]);
-                i++;
-                continue;
+                _putchar('%');
+            }
+            else
+            {
+                write(1, &format[i], 1);
             }
         }
         else if (format[i] == '\\' && format[i+1] == '\n')
