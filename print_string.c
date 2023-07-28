68cf56eef022548461a2c46102946d3c60e581ce
Nada Mogahed <67779643+nadamogahed@users.noreply.github.com>
2023-07-29 02:59:03 +0300

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -1,30 +1,18 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "main.h"
-/*
- * print_string - prints a string
- * @string: an arry pointer
- * @count: count the lenght
- * Return: void
- */
 int print_string(char *string, int count)
 {
-	char *ptr;
-	int j = 0, i;
+    char *ptr;
+    int j = 0, i;
 
-	ptr = (char *) malloc((count + 1) * sizeof(char));
-	if (ptr == NULL)
-		return(0);
+    ptr = (char *) malloc((count + 1) * sizeof(char));
+    if (ptr == NULL)
+        return (0);
 
-	for (j = 0; j < count; j++)
-	{
-		ptr[j] = string[j];
-	}
-	ptr[count] = '\0';
-	i = write(STDOUT_FILENO, ptr, count);
-	free(ptr);
-	return (i);
+    for (j = 0; j < count; j++)
+    {
+        ptr[j] = string[j];
+    }
+    ptr[count] = '\0';
+    i = write(STDOUT_FILENO, ptr, count);
+    free(ptr);
+    return (i);
 }
