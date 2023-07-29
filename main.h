e6d2003136cc95263af6014c974357ff6c3b76a8
Nada Mogahed <67779643+nadamogahed@users.noreply.github.com>
2023-07-29 18:52:01 +0300

    Update main.h

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 int _putchar(char c);
 int print_string(char *string);
-void print_int_c(int x);
+int print_int_c(int x);
 int length(const char *string);
 int _printf(const char *format, ...);
 #endif
