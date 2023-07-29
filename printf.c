e76839320f64e7c64b5e03079cb15c3852af3a03
Nada Mogahed <67779643+nadamogahed@users.noreply.github.com>
2023-07-29 18:48:46 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -27,7 +27,7 @@ int _printf(const char *format, ...)
 			else if (format[i] == '%')
 				len_str += write(1, &format[i], 1);
 			else if (format[i] == 'd' || format[i] == 'i')
-				print_int_c(va_arg(args, int));
+				len_str += print_int_c(va_arg(args, int));
 			else if (format[i] == '\0')
 				len_str += write(1, '\0', 1);
 			else
