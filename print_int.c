92c2928e137f4f5ae544ef207f77db48c29cbe32
Nada Mogahed <67779643+nadamogahed@users.noreply.github.com>
2023-07-29 18:52:24 +0300

    Update print_int.c

--- print_int.c
+++ print_int.c
@@ -4,16 +4,18 @@
  * @x: input integer
  * Return: void
  */
-void print_int_c(int x)
+int print_int_c(int x)
 {
+	int i = 0;
 	if (x < 0)
 	{
-		_putchar('-');
+		i += _putchar('-');
 		x = -x;
 	}
 	if (x / 10 > 0)
 	{
-		_printf("%d", x / 10);
+		i += _printf("%d", x / 10);
 	}
-	_putchar(x % 10 + '0');
+	i += _putchar(x % 10 + '0');
+	return(i);
 }
