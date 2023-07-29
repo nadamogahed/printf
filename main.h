81c9beaef9b1cf6ff3b3d3f73c4ba2c8f62fd950
Nada Mogahed <67779643+nadamogahed@users.noreply.github.com>
2023-07-29 17:15:09 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_string(char *string, int count);
+int _putchar(char *c)
+int print_string(char *string);
 void print_int_c (int x);
 int length(const char *string);
+int _printf(const char *format, ...);
 #endif
 
