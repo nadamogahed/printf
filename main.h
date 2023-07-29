bb824c0b3d0b972dfafc0ef3e4fa7232adf20da0
Nada Mogahed <67779643+nadamogahed@users.noreply.github.com>
2023-07-29 17:28:31 +0300

    Delete main.h

--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c)
-int print_string(char *string);
-void print_int_c (int x);
-int length(const char *string);
-int _printf(const char *format, ...);
-#endif
-
