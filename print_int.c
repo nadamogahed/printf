2a57a11d0976bb8d3fce586cf1909c3b22e3add8
Nada Mogahed <67779643+nadamogahed@users.noreply.github.com>
2023-07-30 03:42:40 +0300

    Update print_int.c

--- print_int.c
+++ print_int.c
@@ -7,16 +7,22 @@
 int print_int_c(int x)
 {
 	int i = 0;
-
-	if (x < 0)
+	if (x == INT_MIN)
+	{
+		i += _putchar('-');
+		i += _putchar('2');
+		x %= 1000000000;
+		x = -x;
+	}
+	else  if (x < 0)
 	{
 		i += _putchar('-');
 		x = -x;
 	}
 	if (x / 10 > 0)
 	{
-		i += _printf("%d", x / 10);
+		i += print_int_c(x / 10);
 	}
 	i += _putchar(x % 10 + '0');
-	return (i);
+	return(i);
 }
